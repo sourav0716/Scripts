e0ec04d6fdec39e78749467423360f0470410bcf
sourav0716 <60028558+sourav0716@users.noreply.github.com>
2020-03-07 18:13:09 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,12 +1,2 @@
 def add(x,y):
     return x+y
-def subtract(x,y):
-    return x-y
-def multiply(x,y):
-    return x*y
-def divide(x,y):
-    pass
-def square(x,y):
-    pass
-def mod(x,y) :
-    pass
